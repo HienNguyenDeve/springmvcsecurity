f9a420bf07dbdc1d6f08010697b5839f5841e8ee
Hien Nguyen <nguyenhien.bds.com@gmail.com>
2024-10-19 10:44:12 +0700

    Step 1: Create Base Project

---
+++ src/main/java/com/hiennguyen/springmvcsecurity/SpringmvcsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.hiennguyen.springmvcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiennguyen/springmvcsecurity/SpringmvcsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiennguyen.springmvcsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
